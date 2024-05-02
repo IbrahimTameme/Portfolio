40077f6bb8330290ec0c60964648fee090fcb051
unknown <hadialsaden@gmail.com>
2024-05-02 03:31:21 +0300

    set config files

--- first-road/components/hero.tsx
+++ first-road/components/hero.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className="flex h-fit w-full">
             <div className=" bg-red-500 w-[50%]">
                 Content</div>
-            <div className=" bg-green-500/50 w-[50%]">
+            <div className=" bg-green-500 w-[50%]">
                 <Image src={'/Images/undraw_complete_design_re_h75h.svg'} width={500} height={500} alt=""/>
             </div>
         </div>
